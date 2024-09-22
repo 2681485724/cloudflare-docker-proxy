c0232ae2d1ada156b590e1c3cd471fb470c332f3
zymlove <2681485724@qq.com>
2024-09-22 23:06:29 +0800

    替换域名libcuda.so为docker-proxy.isirbot.cn

--- README.md
+++ README.md
@@ -30,11 +30,11 @@
    - add more records and modify the config as you need
    ```javascript
    const routes = {
-     "docker.libcuda.so": "https://registry-1.docker.io",
-     "quay.libcuda.so": "https://quay.io",
-     "gcr.libcuda.so": "https://k8s.gcr.io",
-     "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-     "ghcr.libcuda.so": "https://ghcr.io",
+     "docker.docker-proxy.isirbot.cn": "https://registry-1.docker.io",
+     "quay.docker-proxy.isirbot.cn": "https://quay.io",
+     "gcr.docker-proxy.isirbot.cn": "https://k8s.gcr.io",
+     "k8s-gcr.docker-proxy.isirbot.cn": "https://k8s.gcr.io",
+     "ghcr.docker-proxy.isirbot.cn": "https://ghcr.io",
    };
    ```
 
--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.docker-proxy.isirbot.cn": dockerHub,
+  "quay.docker-proxy.isirbot.cn": "https://quay.io",
+  "gcr.docker-proxy.isirbot.cn": "https://gcr.io",
+  "k8s-gcr.docker-proxy.isirbot.cn": "https://k8s.gcr.io",
+  "k8s.docker-proxy.isirbot.cn": "https://registry.k8s.io",
+  "ghcr.docker-proxy.isirbot.cn": "https://ghcr.io",
+  "cloudsmith.docker-proxy.isirbot.cn": "https://docker.cloudsmith.io",
+  "ecr.docker-proxy.isirbot.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker-proxy.isirbot.cn": dockerHub,
 };
 
 function routeByHosts(host) {
